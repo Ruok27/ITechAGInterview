eda3cc825c1cef630a6779de4228b66665e3f876
ruok27 <jakxii27@gmail.com>
2021-05-13 19:20:31 -0400

    exercise 2 complete

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,3 +1,20 @@
-  function resetForm() {
-    document.getElementById("form1").reset();
-  }
+function resetForm() {
+  document.getElementById("form1").reset();
+}
+
+function submitString() {
+  var x = document.getElementById("arrayInput").value.split(" ");
+
+  return x;
+
+  document.getElementById("arrayInput").reset();
+}
+
+function displayString() {
+  var y = submitString();
+
+document.getElementById("displayArrayHere").append(y);
+
+
+
+}
